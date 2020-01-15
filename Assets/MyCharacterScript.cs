883200a2e7ef556473f0b56ad5a72b1cf9abd5b7
clement <clement.opry@ynov.com>
2020-01-15 21:36:00 +0100

    map finished

--- Assets/MyCharacterScript.cs
+++ Assets/MyCharacterScript.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class MyCharacterScript : MonoBehaviour
 {
+    [SerializeField]
     protected Joystick joystick;
 
+    [SerializeField]
     protected ActionButtonScript joybutton;
+
+    [SerializeField]
+    private float Speed;
 
     protected bool action;
 
     // Start is called before the first frame update
     void Start()
     {
-        joystick = FindObjectOfType<Joystick>();
-        joybutton = FindObjectOfType<ActionButtonScript>();
     }
 
     // Update is called once per frame
@@ -22,7 +25,8 @@ public class MyCharacterScript : MonoBehaviour
     {
         var rigibody = GetComponent<Rigidbody>();
 
-        rigibody.velocity = new Vector3(joystick.Horizontal * 100f, rigibody.velocity.y, joystick.Vertical * 100f);
+        rigibody.velocity = new Vector3(joystick.Horizontal * Speed, rigibody.velocity.y, joystick.Vertical * Speed);
+        Debug.Log(rigibody.velocity);
         if(!action && joybutton.Pressed)
         {
             Debug.Log("joybutton");

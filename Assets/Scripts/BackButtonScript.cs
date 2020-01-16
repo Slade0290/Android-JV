32286a919889ed1066e4a0d7acaef4b158e3ac88
Slade0290 <ly.david@live.fr>
2020-01-16 23:26:45 +0100

    almost finish locker room, start tutorial & game over scene

---
+++ Assets/Scripts/BackButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class BackButtonScript : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+
+    public bool Pressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+        SceneManager.LoadScene("Menu");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

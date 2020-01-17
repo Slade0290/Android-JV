b58f5b6b69d15b853e57a2c8084db56b68eb78b0
clement <clement.opry@ynov.com>
2020-01-17 18:23:24 +0100

    add fense animation

---
+++ Assets/Scripts/OpenFense.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenFense : MonoBehaviour
+{
+    private int NbColliderInTrigger;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(NbColliderInTrigger > 0)
+        {
+            anim.Play("OpenFense");
+        }
+        else
+        {
+            anim.Play("CloseFense");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        NbColliderInTrigger++;
+        Debug.Log(other.gameObject.name);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        NbColliderInTrigger--;
+    }
+}

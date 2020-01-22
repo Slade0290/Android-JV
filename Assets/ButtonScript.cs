01e1d1bee8bcd9074ecb7f71191128ecf9336845
Slade0290 <ly.david@live.fr>
2020-01-22 20:37:39 +0100

    adding menu level - not finish yet

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    public void ChangeSceneToSuperMarket()
+    {
+        Debug.Log("Pressed");
+        SceneManager.LoadScene("ChooseLevel");
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerUp");
+        SceneManager.LoadScene("ChooseLevel");
+    }
+
+}

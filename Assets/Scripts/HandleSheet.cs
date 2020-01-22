fee3d8f68c9bc49e9e4d02c0e1f56c404c2c63f8
clement <clement.opry@ynov.com>
2020-01-22 22:08:38 +0100

    color map, starting inventory

---
+++ Assets/Scripts/HandleSheet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleSheet : MonoBehaviour
+{
+    private Animator Anim;
+
+    private ActionButtonScript ButtonA;
+
+    private bool canOpen = false;
+
+    private bool isOpen = false;
+
+    private void Start()
+    {
+        ButtonA = GetComponent<ActionButtonScript>();
+        Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canOpen)
+        {
+            if (isOpen && (ButtonA.Pressed == true))
+            {
+                Debug.Log("fermeture");
+                Anim.Play("CloseSheet");
+                isOpen = false;
+
+            }else if(!isOpen && (ButtonA.Pressed == true))
+            {
+                Debug.Log("ouverture");
+                Anim.Play("OpenSheet");
+                isOpen = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canOpen = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canOpen = false;
+    }
+}
---
+++ Assets/Scripts/InventoryScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class InventoryScript : MonoBehaviour, IPointerDownHandler
+{
+    public bool InventoryIsOpen = false;
+
+    [SerializeField]
+    private GameObject Inventory;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (InventoryIsOpen)
+        {
+            Inventory.SetActive(false);
+            InventoryIsOpen = false;
+        }
+        else
+        {
+            Inventory.SetActive(true);
+            InventoryIsOpen = true;
+        }
+    }
+
+    void Start()
+    {
+        //Inventory.SetActive(false);
+    }
+}

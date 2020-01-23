0830cee68ba50d70e8befb9c565cdc27afbdcd41
Slade0290 <ly.david@live.fr>
2020-01-23 15:59:04 +0100

    adding timer

---
+++ Assets/GameDesign.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameDesign : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject timer;
+
+    private float timeLeft = 60.0f;
+
+    public TextMeshProUGUI text;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        text.text = "Time Left:" + Mathf.Round(timeLeft);
+        if (timeLeft < 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}

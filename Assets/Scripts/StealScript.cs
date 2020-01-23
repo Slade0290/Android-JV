5d0d7d004e1999032a5c18e3faf99c868d5603e2
ClemPota <clement.opry@ynov.com>
2020-01-23 16:24:01 +0100

    can steal

---
+++ Assets/Scripts/StealScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StealScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ButtonA;
+
+    private bool canSteal = false;
+
+    private bool Pressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ButtonA != null)
+        {
+            Pressed = ButtonA.GetComponent<ActionButtonScript>().Pressed;
+            //Debug.Log(Pressed);
+            if (canSteal && Pressed)
+            {
+                CharacterColorPref.score += 50;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if(other.gameObject.tag == "Player")
+        {
+            canSteal = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            canSteal = false;
+    }
+}

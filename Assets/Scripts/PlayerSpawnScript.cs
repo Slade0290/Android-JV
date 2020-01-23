76bdca6d3bff2b74ac349000e54c58d94fa42678
ClemPota <clement.opry@ynov.com>
2020-01-23 14:49:29 +0100

    spawner

---
+++ Assets/Scripts/PlayerSpawnScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawnScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject PlayerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject Player = Instantiate(PlayerPrefab);
+        Player.transform.position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

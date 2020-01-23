f5b17f5380d3b12acf9dff05a4ee8ea83f51a5b8
clement <clement.opry@ynov.com>
2020-01-23 21:58:35 +0100

    handle steal

--- Assets/Scripts/StealScript.cs
+++ Assets/Scripts/StealScript.cs
@@ -13,6 +13,10 @@ public class StealScript : MonoBehaviour
 
     private bool Pressed;
 
+    private GameObject[] Childrens;
+
+    private Component Items;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,14 +34,42 @@ public class StealScript : MonoBehaviour
                 Debug.Log("stole");
                 CharacterColorPref.score += 50;
                 hasSteal = true;
+                Pressed = false;
             }
         }
 
         if (hasSteal)
         {
+            //Items = FindComponentInChildWithTag<Component>(gameObject, "Items");
 
+            //Items.gameObject.SetActive(false);
+
+            Childrens = GetComponentsInChildren<GameObject>();
+            if(Childrens != null)
+            {
+                foreach (var Children in Childrens)
+                {
+                    if (Children.tag == "Items")
+                    {
+                        Children.SetActive(false);
+                    }
+                }
+            }
         }
     }
+
+    /*public Component FindComponentInChildWithTag(this GameObject parent, string tag)
+    {
+        Transform t = parent.transform;
+        foreach (Transform tr in t)
+        {
+            if (tr.tag == tag)
+            {
+                return tr.GetComponent();
+            }
+        }
+        return default;
+    }*/
 
     private void OnTriggerEnter(Collider other)
     {
